0208773f8cb954f8834631518c4c9a825a39761e
dongjun <sdj799@naver.com>
2024-10-21 14:35:27 +0900

    feat: server.js 전체허용으로 변경

--- server/server.js
+++ server/server.js
@@ -3,17 +3,13 @@ const app = express();
 
 const server = require("http").createServer(app);
 const io = require("socket.io")(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: { origin: "*", methods: ["*"] },
 });
 
 app.use(express.static(__dirname + "'../dist'"));
 
 io.on("connection", (socket) => {
-  console.log("소켓 커넥션 발생");
-
   socket.on("message", (message) => {
-    console.log(message);
-
     io.emit("message", message);
   });
 });
