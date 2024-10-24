df27bd6e66ea339b3ae4c5881630da224601838c
dongjun <sdj799@naver.com>
2024-10-24 20:00:44 +0900

    design: 로그인, 회원가입 페이지 마크업

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,8 @@
 import BasicLayout from "layouts/BasicLayout";
 import ClassRoom from "page/ClassRoom";
 import Home from "page/Home";
+import Login from "page/Login";
+import SignUp from "page/SignUp";
 import { createBrowserRouter } from "react-router-dom";
 
 const Router = createBrowserRouter([
@@ -20,7 +22,11 @@ const Router = createBrowserRouter([
 
   {
     path: "/login",
-    element: "login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
   },
 ]);
 export default Router;
