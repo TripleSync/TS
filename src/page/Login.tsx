cff3dd0114f97617a13a93620ea34a3a56d7d35c
dongjun <sdj799@naver.com>
2024-10-29 20:55:55 +0900

    feat: useInput 사용, 버튼 위치 변경

--- src/page/Login.tsx
+++ src/page/Login.tsx
@@ -1,12 +1,13 @@
 import LoginButton from "@components/Login/LoginButton";
 import LoginInput from "@components/Login/LoginInput";
-import React, { useState } from "react";
+import LogoArea from "@components/Login/LogoArea";
+import useInput from "hooks/useInput";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/whitelogo.png";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
   const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -15,32 +16,16 @@ const Login = () => {
 
   return (
     <div className="mx-auto flex h-full overflow-hidden rounded-lg">
-      <div className="flex w-1/2 items-center justify-center">
-        <div className="w-1/2 rounded-full bg-secondary">
-          <img src={logo} alt="logo" />
-        </div>
-      </div>
+      <LogoArea />
       <div className="flex w-1/2 items-center justify-center bg-gray-100">
         <div className="w-full p-8">
           <h2 className="mb-6 text-center text-2xl font-bold">Login</h2>
           <form onSubmit={handleSubmit}>
-            <LoginInput
-              label="Email"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <LoginInput
-              label="Password"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <LoginInput label="Email" type="email" id="email" value={email} onChange={onChangeEmail} />
+            <LoginInput label="Password" type="password" id="password" value={password} onChange={onChangePassword} />
             <div className="mt-8 flex justify-between">
-              <LoginButton text="Login" type="submit" />
               <LoginButton text="Go to Sign Up" onClick={() => navigate("/signup")} />
+              <LoginButton text="OK" type="submit" />
             </div>
           </form>
         </div>
--- src/page/SignUp.tsx
+++ src/page/SignUp.tsx
@@ -1,15 +1,16 @@
 import LoginButton from "@components/Login/LoginButton";
 import LoginInput from "@components/Login/LoginInput";
-import React, { useState } from "react";
+import LogoArea from "@components/Login/LogoArea";
+import useInput from "hooks/useInput";
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import logo from "../assets/whitelogo.png";
 
 const SignUp = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [nickname, setNickname] = useState("");
+  const [name, onChangeName] = useInput("");
+  const [email, onChangeEmail] = useInput("");
+  const [password, onChangePassword] = useInput("");
+  const [phone, onChangePhone] = useInput("");
+  const [nickname, onChangeNickname] = useInput("");
   const navigate = useNavigate();
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -18,47 +19,19 @@ const SignUp = () => {
 
   return (
     <div className="mx-auto flex h-full overflow-hidden rounded-lg">
-      <div className="flex w-1/2 items-center justify-center">
-        <div className="w-1/2 rounded-full bg-secondary">
-          <img src={logo} alt="logo" />
-        </div>
-      </div>
+      <LogoArea />
       <div className="flex w-1/2 items-center justify-center bg-gray-100">
         <div className="w-full p-8">
           <h2 className="mb-6 text-center text-2xl font-bold">Sign Up</h2>
           <form onSubmit={handleSubmit}>
-            <LoginInput label="Name" type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-            <LoginInput
-              label="Email"
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <LoginInput
-              label="Password"
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <LoginInput
-              label="Phone Number"
-              type="tel"
-              id="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-            <LoginInput
-              label="Nickname"
-              type="text"
-              id="nickname"
-              value={nickname}
-              onChange={(e) => setNickname(e.target.value)}
-            />
+            <LoginInput label="Name" type="text" id="name" value={name} onChange={onChangeName} />
+            <LoginInput label="Email" type="email" id="email" value={email} onChange={onChangeEmail} />
+            <LoginInput label="Password" type="password" id="password" value={password} onChange={onChangePassword} />
+            <LoginInput label="Phone Number" type="tel" id="phone" value={phone} onChange={onChangePhone} />
+            <LoginInput label="Nickname" type="text" id="nickname" value={nickname} onChange={onChangeNickname} />
             <div className="mt-8 flex justify-between">
-              <LoginButton text="Sign Up" type="submit" />
               <LoginButton text="Go to Login" onClick={() => navigate("/login")} />
+              <LoginButton text="OK" type="submit" />
             </div>
           </form>
         </div>
